cba6798b9fb37ada414456569da9e8d28c99df5e
Thanh Pham <thanh1.pham@lge.com>
2021-01-06 12:50:56 +0700

    Initialize main and Tkinter

---
+++ extract data/appmain.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+
+class AppMain:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.mainloop()
---
+++ extract data/main.py
@@ -0,0 +1,4 @@
+from appmain import AppMain
+
+if __name__ == '__main__':
+    appMain = AppMain()
